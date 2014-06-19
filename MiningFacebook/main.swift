c408a3509c848f86d02621ae6517b6311decd34a
Cyrix <freeflyworld@cyrixhero.net>
2014-06-20 00:25:13 +0800

    Initial Commit

---
+++ MiningFacebook/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiningFacebook
+//
+//  Created by 湛昆峰 on 2014/6/20.
+//  Copyright (c) 2014年 湛昆峰. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
