d00b8410fb8a8f334d41d00eb21a159c19c66651
Cyrix <freeflyworld@cyrixhero.net>
2014-06-24 00:07:30 +0800

    Coding: func AnalyticsJSON_Likes() and func AnalyticsJSON_SharedPosts()

--- MiningFacebook/Analytics.swift
+++ MiningFacebook/Analytics.swift
@@ -13,8 +13,20 @@ func get_urlPath(getPostID: String, getAccessToken: String
     return (urlPath_Likes, urlPath_SharedPosts)
 }
 
-//func AnalyticsJSON(test: NSData) -> (json: NSDictionary) {
-//    var json = NSJSONSerialization.JSONObjectWithData(test, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-//    return json
-//}
+func AnalyticsJSON_Likes(getURLPath_Likes: String) -> (JSONData_Likes: NSDictionary) {
+    var url_Likes: NSURL = NSURL(string: getURLPath_Likes)
+    var URLRequest: NSURLRequest = NSURLRequest(URL: url_Likes)
+    var DataResponse: NSData = NSURLConnection.sendSynchronousRequest(URLRequest, returningResponse: nil, error: nil)
+    var JSONData_Likes = NSJSONSerialization.JSONObjectWithData(DataResponse, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
 
+    return JSONData_Likes
+}
+
+func AnalyticsJSON_SharedPosts(getURLPath_SharedPosts: String) -> (JSONData_SharedPosts: NSDictionary) {
+    var url_SharedPosts: NSURL = NSURL(string: getURLPath_SharedPosts)
+    var URLRequest: NSURLRequest = NSURLRequest(URL: url_SharedPosts)
+    var DataResponse: NSData = NSURLConnection.sendSynchronousRequest(URLRequest, returningResponse: nil, error: nil)
+    var JSONData_SharedPosts = NSJSONSerialization.JSONObjectWithData(DataResponse, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+    
+    return JSONData_SharedPosts
+}
--- MiningFacebook/MiningPost.swift
+++ MiningFacebook/MiningPost.swift
@@ -4,17 +4,13 @@ func MiningPost() {
     println("AccessToken: ")
     var AccessToken = input()
 
-    var url_Likes: NSURL
-    var url_SharedPosts: NSURL
-    var URLRequest: NSURLRequest
-    var DataResponse: NSData
     var JSONData: NSDictionary
     var FacebookUID: NSArray
     var outputFile: NSMutableString = ""
     var getNextFlag: NSDictionary
     var nextFlag: NSArray
     var FileName: String
-    var terminate: String    
+    var terminate: String
 
     do {
         println("{Post-ID}: ")
@@ -22,17 +18,15 @@ func MiningPost() {
         var AccessType = get_urlPath(PostID, AccessToken)
         var urlPath_Likes = AccessType.urlPath_Likes
         var urlPath_SharedPosts = AccessType.urlPath_SharedPosts
-        
+
         do {
-            url_Likes = NSURL(string: urlPath_Likes)
-            URLRequest = NSURLRequest(URL: url_Likes)
-            DataResponse = NSURLConnection.sendSynchronousRequest(URLRequest, returningResponse: nil, error: nil)
-            JSONData = NSJSONSerialization.JSONObjectWithData(DataResponse, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            JSONData = AnalyticsJSON_Likes(urlPath_Likes)
             FacebookUID = JSONData["data"].valueForKey("id") as NSArray
-            
+
             for var i = 0; i < FacebookUID.count; ++i {
                 outputFile.appendFormat("%@\n", FacebookUID.objectAtIndex(i).stringByStandardizingPath)
             }
+    
             if FacebookUID.count < 1000 {
                 break
             }else {
@@ -45,15 +39,13 @@ func MiningPost() {
         } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
 
         do {
-            url_SharedPosts = NSURL(string: urlPath_SharedPosts)
-            URLRequest = NSURLRequest(URL: url_SharedPosts)
-            DataResponse = NSURLConnection.sendSynchronousRequest(URLRequest, returningResponse: nil, error: nil)
-            JSONData = NSJSONSerialization.JSONObjectWithData(DataResponse, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            JSONData = AnalyticsJSON_SharedPosts(urlPath_SharedPosts)
             FacebookUID = JSONData["data"].valueForKey("from") as NSArray
         
             for var i = 0; i < FacebookUID.count; ++i {
                 outputFile.appendFormat("%@\n", FacebookUID.objectAtIndex(i).valueForKey("id").stringByStandardizingPath)
             }
+            
             if FacebookUID.count < 1000 {
                 break
             }else {
