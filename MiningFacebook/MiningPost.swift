aa8c76c45f2fa02b69d313b643949fa0f970d1aa
Cyrix <freeflyworld@cyrixhero.net>
2014-09-01 16:10:50 +0800

    Bug fixed

--- MiningFacebook/MiningPost.swift
+++ MiningFacebook/MiningPost.swift
@@ -48,7 +48,7 @@ func MiningPost() {
                 outputFile.appendFormat(AnalyticsData.storeUID_SharedPosts)
                 FacebookUID = AnalyticsData.FacebookUID_Flag
                 
-                if FacebookUID < 1000 {
+                if FacebookUID < 900 {
                     break
                 }else {
                     nextFlag = AnalyticsData.nextFlag
@@ -85,7 +85,7 @@ func MiningPost() {
                 outputFile.appendFormat(AnalyticsData.storeUID_SharedPosts)
                 FacebookUID = AnalyticsData.FacebookUID_Flag
                 
-                if FacebookUID < 1000 {
+                if FacebookUID < 900 {
                     break
                 }else {
                     nextFlag = AnalyticsData.nextFlag
