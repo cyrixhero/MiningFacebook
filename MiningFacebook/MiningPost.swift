1b1e044fc89cadb3ee62c7f41f1dcd4e75d0337b
Cyrix <freeflyworld@cyrixhero.net>
2014-09-01 14:43:24 +0800

    Add Switch case

--- MiningFacebook/MiningPost.swift
+++ MiningFacebook/MiningPost.swift
@@ -8,43 +8,96 @@ func MiningPost() {
     var outputFile: NSMutableString = ""
     var nextFlag: NSArray
     var terminate: String
-
-    do {
-        println("{Post-ID}: ")
-        var PostID = input()
-        var AccessType = get_urlPath(PostID, AccessToken)
-        var urlPath_Likes = AccessType.urlPath_Likes
-        var urlPath_SharedPosts = AccessType.urlPath_SharedPosts
-
-        do {
-            var AnalyticsData = AnalyticsJSON_Likes(urlPath_Likes)
-            outputFile.appendFormat(AnalyticsData.storeUID_Likes)
-            FacebookUID = AnalyticsData.FacebookUID_Flag
     
-            if FacebookUID < 1000 {
-                break
-            }else {
-                nextFlag = AnalyticsData.nextFlag
-                urlPath_Likes = AnalyticsData.NextURLPath_Likes
-            }
-        } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
-
+    println("\nWhich mode do you want? (1.Likes 2.Shared 3.LikeShared)")
+    var mode = input()
+    
+    switch mode {
+    case "1":
         do {
-            var AnalyticsData = AnalyticsJSON_SharedPosts(urlPath_SharedPosts)
-            outputFile.appendFormat(AnalyticsData.storeUID_SharedPosts)
-            FacebookUID = AnalyticsData.FacebookUID_Flag
+            println("{Post-ID}: ")
+            var PostID = input()
+            var AccessType = get_urlPath(PostID, AccessToken)
+            var urlPath_Likes = AccessType.urlPath_Likes
             
-            if FacebookUID < 1000 {
-                break
-            }else {
-                nextFlag = AnalyticsData.nextFlag
-                urlPath_SharedPosts = AnalyticsData.NextURLPath_SharedPosts
-            }
-        } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
-        
-        println("Exit?")
-        terminate = input()
-    } while terminate == "n"
+            do {
+                var AnalyticsData = AnalyticsJSON_Likes(urlPath_Likes)
+                outputFile.appendFormat(AnalyticsData.storeUID_Likes)
+                FacebookUID = AnalyticsData.FacebookUID_Flag
+                
+                if FacebookUID < 1000 {
+                    break
+                }else {
+                    nextFlag = AnalyticsData.nextFlag
+                    urlPath_Likes = AnalyticsData.NextURLPath_Likes
+                }
+            } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
+            
+            println("Exit? (You are in Likes mode now)")
+            terminate = input()
+        } while terminate == "n"
+    case "2":
+        do {
+            println("{Post-ID}: ")
+            var PostID = input()
+            var AccessType = get_urlPath(PostID, AccessToken)
+            var urlPath_SharedPosts = AccessType.urlPath_SharedPosts
+            
+            do {
+                var AnalyticsData = AnalyticsJSON_SharedPosts(urlPath_SharedPosts)
+                outputFile.appendFormat(AnalyticsData.storeUID_SharedPosts)
+                FacebookUID = AnalyticsData.FacebookUID_Flag
+                
+                if FacebookUID < 1000 {
+                    break
+                }else {
+                    nextFlag = AnalyticsData.nextFlag
+                    urlPath_SharedPosts = AnalyticsData.NextURLPath_SharedPosts
+                }
+            } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
+            
+            println("Exit? (You are in Shared mode now)")
+            terminate = input()
+        } while terminate == "n"
+    case "3":
+        do {
+            println("{Post-ID}: ")
+            var PostID = input()
+            var AccessType = get_urlPath(PostID, AccessToken)
+            var urlPath_Likes = AccessType.urlPath_Likes
+            var urlPath_SharedPosts = AccessType.urlPath_SharedPosts
 
+            do {
+                var AnalyticsData = AnalyticsJSON_Likes(urlPath_Likes)
+                outputFile.appendFormat(AnalyticsData.storeUID_Likes)
+                FacebookUID = AnalyticsData.FacebookUID_Flag
+        
+                if FacebookUID < 1000 {
+                    break
+                }else {
+                    nextFlag = AnalyticsData.nextFlag
+                    urlPath_Likes = AnalyticsData.NextURLPath_Likes
+                }
+            } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
+
+            do {
+                var AnalyticsData = AnalyticsJSON_SharedPosts(urlPath_SharedPosts)
+                outputFile.appendFormat(AnalyticsData.storeUID_SharedPosts)
+                FacebookUID = AnalyticsData.FacebookUID_Flag
+                
+                if FacebookUID < 1000 {
+                    break
+                }else {
+                    nextFlag = AnalyticsData.nextFlag
+                    urlPath_SharedPosts = AnalyticsData.NextURLPath_SharedPosts
+                }
+            } while nextFlag[1] as NSString == "next" || nextFlag[2] as NSString == "next"
+            
+            println("Exit? (You are in LikeShared mode now)")
+            terminate = input()
+        } while terminate == "n"
+    default:
+        break
+    }
     SaveToFile(outputFile)
 }
